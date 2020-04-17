f7c64271cd9dc3f3fdf8c9ec8aac1bd8dc3113f3
Abu Ubaida <abu.ubaida1206@gmail.com>
2020-04-17 22:03:45 +0600

    updated

--- B. Strings Equalization.cpp
+++ B. Strings Equalization.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    int q;
+    cin>>q;
+    while(q!=0)
+    {
+        string s,t;
+        cin>>s>>t;
+
+        int flag=0;
+        for(int i=0;i<s.length();i++)
+        {
+            for(int j=0;j<t.length();j++)
+            {
+                if(t[j]==s[i])
+                {
+                    flag=1;
+                    break;
+                }
+            }
+        }
+        if(flag==1)
+            cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+        q--;
+    }
+}
